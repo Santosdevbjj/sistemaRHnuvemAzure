739b8144fe966c8e7bfc0b5862a08abc7eb355c9
Sergio <santossergiorealbjj@outlook.com>
2025-12-05 05:38:56 -0300

    Create scripts_seeds.sql

---
+++ Docs/scripts_seeds.sql
@@ -0,0 +1,8 @@
+-- Seeds de JornadaTrabalho (Azure SQL Database)
+
+INSERT INTO JornadaTrabalho (Nome, HoraEntrada, HoraSaida, DuracaoIntervaloMin, PermiteHorarioNoturno)
+VALUES
+('07:00 às 13:15', '07:00', '13:15', 15, 0),
+('08:00 às 14:15', '08:00', '14:15', 15, 0),
+('09:00 às 15:15', '09:00', '15:15', 15, 0),
+('
